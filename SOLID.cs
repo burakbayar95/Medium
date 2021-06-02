50e82ae1903cdd898a95177f5c6cbf19dea33cfa
Burak <95burakbayar@gmail.com>
2021-06-03 02:25:09 +0300

    SOLID

---
+++ SOLID.cs
@@ -0,0 +1,16 @@
+public class Sepet
+ {
+ public void SepeteEkle()
+ {
+ //Sepete ekleme işlemini yapan kod bloğu
+ }
+ 
+ public void SepettenCikar()
+ {
+ //Sepetten çıkarma işlemini yapan kod bloğu
+ }
+ public void SiparisOlustur()
+ {
+ //Sipariş oluşturma işlemini yapan kod bloğu
+ }
+ }
